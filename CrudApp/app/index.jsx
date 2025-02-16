8f808a47c2fdefd822ac544ab7477959f3a44256
kasDev18 <87753378+kasDev18@users.noreply.github.com>
2025-02-16 17:54:01 +0800

    lesson-6

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,24 +1,47 @@
-import { StyleSheet, Text, View, TextInput, Pressable, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  FlatList,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useState, useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
+import Octicons from "@expo/vector-icons/Octicons";
 
 import { data } from "@/data/todos";
 
 export default function Index() {
   const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
   const [text, setText] = useState("");
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
+
+  const [loaded, error] = useFonts({
+    Inter_500Medium,
+  });
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  const styles = createStyles(theme, colorScheme);
 
   const addTodo = () => {
     if (text.trim()) {
-      const newID = todos.length > 0 ? todos[todos.length - 1].id + todos.length : 1;
+      const newID =
+        todos.length > 0 ? todos[todos.length - 1].id + todos.length : 1;
       setTodos([{ id: newID, title: text, completed: false }, ...todos]);
       setText("");
     }
   };
 
-  console.log(todos);
-  
+  // console.log(todos);
 
   const toggleTodo = (id) => {
     setTodos(
@@ -41,7 +64,12 @@ export default function Index() {
         {item.title}
       </Text>
       <Pressable onPress={() => removeTodo(item.id)}>
-        <MaterialCommunityIcons name="delete-circle" size={36} color="red" selectable={undefined} />
+        <MaterialCommunityIcons
+          name="delete-circle"
+          size={36}
+          color="red"
+          selectable={undefined}
+        />
       </Pressable>
     </View>
   );
@@ -52,81 +80,109 @@ export default function Index() {
         <TextInput
           style={styles.input}
           placeholder="Add a new todo"
-          placeholderTextColor='gray'
+          placeholderTextColor="gray"
           value={text}
           onChangeText={setText}
         />
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
+        <Pressable
+          onPress={() =>
+            setColorScheme(colorScheme === "light" ? "dark" : "light")
+          }
+          style={{ marginLeft: 10 }}
+        >
+          {colorScheme === "dark" ? (
+            <Octicons
+              name="moon"
+              size={24}
+              color={theme.text}
+              selectable={undefined}
+              style={{ width: 36 }}
+            />
+          ) : (
+            <Octicons
+              name="sun"
+              size={24}
+              color={theme.text}
+              selectable={undefined}
+              style={{ width: 36 }}
+            />
+          )}
+        </Pressable>
       </View>
 
       <FlatList
         data={todos}
         renderItem={renderItem}
-        keyExtractor={todo => todo.id}  
+        keyExtractor={(todo) => todo.id}
         contentContainerStyle={{ flexGrow: 1 }}
       ></FlatList>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    padding: 10,
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto',
-    // backgroundColor: 'green'
-  },
-  input: {
-    flex: 1,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    fontSize: 18,
-    minWidth: 0,
-    color: 'white',
-  },
-  addButton: {
-    backgroundColor: 'white',
-    borderRadius: 5,
-    padding: 10,
-  },
-  addButtonText: {
-    fontSize: 18,
-    color: 'black',
-  },
-  todoItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    gap: 4,
-    padding: 10,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto',
-  },
-  todoText: {
-    flex: 1,
-    fontSize: 18,
-    color: 'white',
-  },
-  completedText: {
-    textDecorationLine: 'line-through',
-    color: 'gray',
-  }
-})
+function createStyles(theme, colorScheme) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    inputContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+      padding: 10,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+      // backgroundColor: 'green'
+    },
+    input: {
+      flex: 1,
+      borderColor: "gray",
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      fontSize: 18,
+      fontFamily: "Inter_500Medium",
+      minWidth: 0,
+      color: theme.text,
+    },
+    addButton: {
+      backgroundColor: theme.button,
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText: {
+      fontSize: 18,
+      color: colorScheme === "dark" ? "black" : "white",
+    },
+    todoItem: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: 4,
+      padding: 10,
+      borderBottomColor: "gray",
+      borderBottomWidth: 1,
+      width: "100%",
+      maxWidth: 1024,
+      marginHorizontal: "auto",
+      pointerEvents: "auto",
+    },
+    todoText: {
+      flex: 1,
+      fontSize: 18,
+      fontFamily: "Inter_500Medium",
+      color: theme.text,
+    },
+    completedText: {
+      textDecorationLine: "line-through",
+      color: "gray",
+    },
+  });
+}
